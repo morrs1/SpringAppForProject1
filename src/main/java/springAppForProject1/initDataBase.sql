d4c52c24419af590c47f91aa8286b843382a75b5
morrs <grishkovea@gmail.com>
2024-10-12 00:08:44 +0300

    fix: database add constraint

---
+++ src/main/java/springAppForProject1/initDataBase.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS Человек, Книга;
+
+CREATE TABLE Человек
+(
+    person_id     SERIAL PRIMARY KEY,
+    name          VARCHAR(100)                                    NOT NULL,
+    surname       VARCHAR(100)                                    NOT NULL,
+    year_of_birth INT CHECK (year_of_birth BETWEEN 1920 AND 2006) NOT NULL,
+    UNIQUE (name, surname)
+);
+
+CREATE TABLE Книга
+(
+    book_id   SERIAL PRIMARY KEY,
+    name      VARCHAR(100) NOT NULL,
+    author    VARCHAR(100) NOT NULL,
+    year      INT CHECK (year BETWEEN 1700 AND 2024),
+    person_id INT,
+    FOREIGN KEY (person_id) REFERENCES Человек (person_id),
+    UNIQUE (name, author)
+);
+
+INSERT INTO Человек (name, surname, year_of_birth)
+VALUES ('Иван', 'Иванов', 1985),
+       ('Петр', 'Петров', 1990),
+       ('Светлана', 'Сидорова', 1978),
+       ('Анна', 'Антонова', 2000),
+       ('Дмитрий', 'Дмитриев', 1982),
+       ('Елена', 'Еленина', 1995),
+       ('Алексей', 'Алексеев', 1970),
+       ('Мария', 'Маркова', 1988),
+       ('Николай', 'Николаев', 1965),
+       ('Ольга', 'Ольгина', 2001);
+
+
+INSERT INTO Книга (name, author, year)
+VALUES ('Война и мир', 'Лев Толстой', 1869),
+       ('1984', 'Джордж Оруэлл', 1949),
+       ('Мастер и Маргарита', 'Михаил Булгаков', 1967),
+       ('Преступление и наказание', 'Федор Достоевский', 1866),
+       ('Гарри Поттер и философский камень', 'Дж.К. Роулинг', 1997),
+       ('Убить пересмешника', 'Харпер Ли', 1960),
+       ('Тихий Дон', 'Михаил Шолохов', 1928),
+       ('Собачье сердце', 'Михаил Булгаков', 1925),
+       ('Анна Каренина', 'Лев Толстой', 1877),
+       ('Маленький принц', 'Антуан де Сент-Экзюпери', 1943);
