d4227cccd1032f59fc85501ee894711f7321d2c2
morrs <grishkovea@gmail.com>
2024-10-12 23:49:30 +0300

    feature: added personDAO and readAll in there

---
+++ src/main/java/springAppForProject1/DAO/PersonDAO.java
@@ -0,0 +1,24 @@
+package springAppForProject1.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import springAppForProject1.models.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<Person> readAll() {
+       return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
