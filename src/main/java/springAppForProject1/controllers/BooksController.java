46ae8361e45c707d29d7a2a267a638fa821617f5
morrs <grishkovea@gmail.com>
2024-10-12 18:02:17 +0300

    feature: add endpoint of show book

--- src/main/java/springAppForProject1/controllers/BooksController.java
+++ src/main/java/springAppForProject1/controllers/BooksController.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springAppForProject1.DAO.BookDAO;
 import springAppForProject1.models.Book;
+import springAppForProject1.models.Person;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BooksController {
 
     private final BookDAO bookDAO;
@@ -23,6 +27,19 @@ public class BooksController {
     @GetMapping("")
     public String readAll(Model model) {
         model.addAttribute("books", bookDAO.readAll());
-        return "index";
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String read(@PathVariable("id") int id, Model model, @ModelAttribute("book") Book book) {
+        model.addAttribute("book", bookDAO.read(id));
+
+        Optional<Person> bookOwner = bookDAO.getBookOwner(id);
+
+        bookOwner.ifPresent(person -> model.addAttribute("owner", person));
+//        else
+//            model.addAttribute("people", personDAO.index());
+
+        return "books/show";
     }
 }
