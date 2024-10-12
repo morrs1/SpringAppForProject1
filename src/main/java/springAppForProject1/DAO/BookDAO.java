d7dca5ef1537319552e7fc82e3f8691f1bdd5efb
morrs <grishkovea@gmail.com>
2024-10-12 17:46:26 +0300

    feature: add methods for read(id) and getBookOwner

--- src/main/java/springAppForProject1/DAO/BookDAO.java
+++ src/main/java/springAppForProject1/DAO/BookDAO.java
@@ -5,8 +5,10 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import springAppForProject1.models.Book;
+import springAppForProject1.models.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -17,8 +19,24 @@ public class BookDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Book> readAll(){
+    public List<Book> readAll() {
         return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book read(int id) {
+        return jdbcTemplate.query(
+                "SELECT * FROM book WHERE book_id=?",
+                new BeanPropertyRowMapper<>(Book.class),
+                new Object[]{id}
+        ).stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> getBookOwner(int id) {
+        // Выбираем все колонки таблицы Person из объединенной таблицы
+        return jdbcTemplate.query("SELECT Person.* FROM Book JOIN Person ON Book.person_id = Person.person_id " +
+                                "WHERE Book.book_id = ?", preparedStatement -> preparedStatement.setInt(1, id),
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
     }
 
 }
